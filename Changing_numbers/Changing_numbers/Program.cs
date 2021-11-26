9f1497513fc23cd535a6e034f1cdca8e34be4fca
Elman N. Naghdaliyev <elmannn@code.edu.az>
2021-11-26 09:13:41 +0400

    First c# task

---
+++ Changing_numbers/Changing_numbers/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Changing_numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 5;
+            int m = 11;
+            n = n + m;
+            m = n + m;
+            n = m - n;
+            m = m - n - n;
+
+            Console.WriteLine(n + " " + m);
+        }
+    }
+}
